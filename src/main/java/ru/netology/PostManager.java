3a3efbcfe7dec566be5bbf6902a6b805bc9453bd
mfv <mit.fedor@gmail.com>
2020-09-28 18:17:15 +0500

    add PostManager class

---
+++ src/main/java/ru/netology/PostManager.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+public class PostManager {
+    private Post[] post;
+
+    public Post[] searchPost(long groupID, String query, long ownerID, long count) {
+        return null;
+    }
+
+    public void deletePost(long groupID, long postID) {
+
+    }
+}
